f23540b0fc0f441fb48bc1e3bad5f8fac31e0044
Tin Le - Home <levinhtin@live.com>
2017-04-10 23:37:18 +0700

    Initial

---
+++ NotificationSignalR/NotificationSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NotificationSignalR.Startup))]
+
+namespace NotificationSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
